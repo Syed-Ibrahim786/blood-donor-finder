d2f82768897be267f5844169bf9eabe533d87bcd
codezer000 <lijindavid18@gmail.com>
2025-07-06 14:04:28 +0530

    navbar updated -v0.0.1

--- donorFinder/src/routes/Approutes.jsx
+++ donorFinder/src/routes/Approutes.jsx
@@ -7,14 +7,22 @@ import Login from '../page/Home/Login/Login'
 import Register from '../page/Home/Login/Register'
 import { AnimatePresence , motion } from 'framer-motion'
 import { useLocation } from 'react-router-dom'
+import Search from '../page/Search'
 
 
 function Approutes() {
+
+    // UseLocal has the current location .. it is important for framer motion and other animation librariues to work while routing back to back from pages to pages
 
     const location = useLocation()
 
   return (
     <>
+
+    {/* AnimationPresence is a framer motion thing wrap this component around other  roters that you want to animate ..... mode= wait is for telling the roter to wait until the animation is finished */}
+
+    {/* only pass location and pathname when you wants to animater b/w router */}
+
 
         <AnimatePresence mode='wait'>
 
@@ -22,24 +30,25 @@ function Approutes() {
 
             <Route path='/' element={<MainLayout/>}/>
             <Route index element={<Home/>}/>
+            <Route path='/search' element={<Search/>}></Route>
 
 
             <Route path='/login' element={  <motion.div
             
-            initial={{opacity:0, y:30}}
-            animate={{opacity:1,y: 0}}
+            initial={{opacity:0, y:30 , x:-30}}
+            animate={{opacity:1,y: 0 , x:0}}
             exit={{opacity:0}}
-            transition={{duration:0.3}}
+            transition={{duration:0.2}}
             
             > <Login/></motion.div>}/>
 
 
             <Route path='/register' element={  <motion.div
             
-            initial={{opacity:0, y:30 }}
-            animate={{opacity:1,y:0}}
+            initial={{opacity:0, y:30 , x:30 }}
+            animate={{opacity:1,y:0 , x:0}}
             exit={{opacity:0}}
-            transition={{duration:0.3}}
+            transition={{duration:0.2}}
             
             > <Register/></motion.div>}/>
 
